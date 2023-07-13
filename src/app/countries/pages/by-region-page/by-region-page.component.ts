baaeea7016971f334f6c54f686c83b9800cdf93a
rivera.hidalgo.antonio.jose <rivera.hidalgo.antonio.jose@gmail.com>
2023-07-13 10:01:52 +0200

    Country app

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
+import { Region } from '../../interfaces/region.type';
+
+
+
 
 @Component({
   selector: 'app-by-region-page',
@@ -8,10 +12,20 @@ import { Country } from '../../interfaces/country';
   styles: [
   ]
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public countries: Country [] = [];
+  public regions: Region[] = ['Africa','Americas','Asia','Europe','Oceania'];
+  public selectedRegion?: Region;
+
+
   constructor(private countriesService: CountriesService) { }
-  searchByRegion(region: string):void {
+
+  ngOnInit(): void {
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+  }
+  searchByRegion(region: Region):void {
+    this.selectedRegion = region;
     this.countriesService.searchRegion(region)
     .subscribe(countries => {
       this.countries = countries;
